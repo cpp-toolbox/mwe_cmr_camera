c6ec7d55b09e295734e960ba0d1514b0f8a1d432
cuppajoeman <ccn@cuppajoeman.com>
2025-03-08 03:34:36 -0500

    feat: working visualization of path and better indicators

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include "input/input_state/input_state.hpp"
 
 #include "utility/fixed_frequency_loop/fixed_frequency_loop.hpp"
+#include "utility/unique_id_generator/unique_id_generator.hpp"
+#include "utility/print_utils/print_utils.hpp"
 
 #include "graphics/vertex_geometry/vertex_geometry.hpp"
 #include "graphics/shader_standard/shader_standard.hpp"
@@ -14,7 +16,6 @@
 #include "graphics/fps_camera/fps_camera.hpp"
 #include "graphics/window/window.hpp"
 #include "graphics/colors/colors.hpp"
-#include "utility/unique_id_generator/unique_id_generator.hpp"
 
 #include <iostream>
 
@@ -56,9 +57,6 @@ int main() {
     GLFWLambdaCallbackManager glcm(window.glfw_window, char_callback, key_callback, mouse_pos_callback,
                                    mouse_button_callback, frame_buffer_size_callback);
 
-    auto ball = vertex_geometry::generate_icosphere(3, 1);
-    Transform ball_transform;
-
     glm::mat4 identity = glm::mat4(1);
     shader_cache.set_uniform(ShaderType::CWL_V_TRANSFORMATION_UBOS_1024_WITH_SOLID_COLOR,
                              ShaderUniformVariable::CAMERA_TO_CLIP,
@@ -78,6 +76,24 @@ int main() {
     glBufferData(GL_UNIFORM_BUFFER, sizeof(ltw_matrices), ltw_matrices, GL_STATIC_DRAW);
     glBindBufferBase(GL_UNIFORM_BUFFER, 0, ltw_matrices_gl_name);
 
+    auto cam_indicator_ivp = vertex_geometry::generate_cylinder(16, 1, .12);
+    vertex_geometry::translate_vertices_in_place(cam_indicator_ivp.xyz_positions, glm::vec3(0, -1, 0));
+    auto cone = vertex_geometry::generate_cone(16, 1, .25);
+    vertex_geometry::merge_ivps(cam_indicator_ivp, cone);
+    vertex_geometry::rotate_vertices_in_place(cam_indicator_ivp.xyz_positions, glm::vec3(.25, .25, 0));
+    draw_info::TransformedIVPGroup cam_indicator_tig_for_copying({cam_indicator_ivp}, ltw_id_generator.get_id());
+    std::vector<draw_info::TransformedIVPGroup> cam_indicators;
+
+    int spline_ltw_id = ltw_id_generator.get_id();
+    std::unique_ptr<draw_info::IndexedVertexPositions> spline_ivp;
+
+    std::vector<std::pair<glm::vec3, glm::vec3>> fake_spline_pos_and_tangent_vec;
+
+    int ball_ltw_id = ltw_id_generator.get_id();
+    /*auto ball = vertex_geometry::generate_icosphere(3, 1);*/
+    auto ball = vertex_geometry::generate_cylinder(8, 1, 1);
+    Transform ball_transform;
+
     std::function<void(double)> tick = [&](double dt) {
         /*glfwGetFramebufferSize(window, &width, &height);*/
 
@@ -90,16 +106,72 @@ int main() {
                                  input_state.is_pressed(EKey::s), input_state.is_pressed(EKey::d),
                                  input_state.is_pressed(EKey::SPACE), input_state.is_pressed(EKey::LEFT_SHIFT), dt);
 
+        if (input_state.is_just_pressed(EKey::q)) {
+            auto cam_indicator_tig_copy = cam_indicator_tig_for_copying;
+            cam_indicator_tig_copy.id = ltw_id_generator.get_id();
+            for (auto &ivp : cam_indicator_tig_copy.ivps) {
+                ivp.id =
+                    batcher.cwl_v_transformation_ubos_1024_with_solid_color_shader_batcher.object_id_generator.get_id();
+            }
+            cam_indicator_tig_copy.transform = fps_camera.transform;
+            // TODO: figure out why think has to dowith the way Look at works opposite movement somethin something
+            cam_indicator_tig_copy.transform.rotation *= -1;
+            p(cam_indicator_tig_copy.transform);
+            cam_indicators.push_back(cam_indicator_tig_copy);
+
+            fake_spline_pos_and_tangent_vec.push_back(
+                {cam_indicator_tig_copy.transform.position, fps_camera.transform.compute_forward_vector()});
+        }
+
+        if (input_state.is_just_pressed(EKey::p)) {
+            p("making spline now");
+            spline_ivp = std::make_unique<draw_info::IndexedVertexPositions>(
+                vertex_geometry::generate_segmented_cylinder(fake_spline_pos_and_tangent_vec, 0.5f, 8));
+
+            /*std::function<glm::vec3(double)> f = [](double t) { return glm::vec3(std::cos(t), std::sin(t), t); };*/
+            /**/
+            /*double t_start = 0.0;*/
+            /*double t_end = 10.0;*/
+            /*double step_size = 0.1;*/
+            /*double finite_diff_delta = 1e-5;*/
+            /**/
+            /*spline_ivp = std::make_unique<draw_info::IndexedVertexPositions>(*/
+            /*    vertex_geometry::generate_function_visualization(f, t_start, t_end, step_size, finite_diff_delta));*/
+            /*p(*spline_ivp);*/
+        }
+
         shader_cache.set_uniform(ShaderType::CWL_V_TRANSFORMATION_UBOS_1024_WITH_SOLID_COLOR,
                                  ShaderUniformVariable::WORLD_TO_CAMERA, fps_camera.get_view_matrix());
 
-        std::vector<unsigned int> ltw_indices(ball.xyz_positions.size(), 0);
-        batcher.cwl_v_transformation_ubos_1024_with_solid_color_shader_batcher.queue_draw(
-            0, ball.indices, ball.xyz_positions, ltw_indices);
+        // DRAWING START
+        if (spline_ivp) {
+            std::vector<unsigned int> ltw_indices(spline_ivp->xyz_positions.size(), spline_ltw_id);
+            batcher.cwl_v_transformation_ubos_1024_with_solid_color_shader_batcher.queue_draw(
+                spline_ivp->id, spline_ivp->indices, spline_ivp->xyz_positions, ltw_indices);
+            ltw_matrices[spline_ltw_id] = glm::mat4(1); // doesn't move
+        }
 
-        ltw_matrices[0] = ball_transform.get_transform_matrix();
+        std::vector<unsigned int> ltw_indices(ball.xyz_positions.size(), ball_ltw_id);
+        batcher.cwl_v_transformation_ubos_1024_with_solid_color_shader_batcher.queue_draw(
+            ball.id, ball.indices, ball.xyz_positions, ltw_indices);
+
+        ltw_matrices[ball_ltw_id] = ball_transform.get_transform_matrix();
+
+        for (const auto &cam_indicator_tig : cam_indicators) {
+            for (const auto &ivp : cam_indicator_tig.ivps) {
+                std::vector<unsigned int> ltw_indices(ivp.xyz_positions.size(), cam_indicator_tig.id);
+                batcher.cwl_v_transformation_ubos_1024_with_solid_color_shader_batcher.queue_draw(
+                    ivp.id, ivp.indices, ivp.xyz_positions, ltw_indices);
+            }
+
+            /*p("tiggy");*/
+            /*p(cam_indicator_tig.transform);*/
+            ltw_matrices[cam_indicator_tig.id] = cam_indicator_tig.transform.get_transform_matrix();
+        }
 
         batcher.cwl_v_transformation_ubos_1024_with_solid_color_shader_batcher.draw_everything();
+
+        // DRAWING END
 
         glBindBuffer(GL_UNIFORM_BUFFER, ltw_matrices_gl_name);
         glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(ltw_matrices), ltw_matrices);
